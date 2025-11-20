efd06ad1ad3e40abfd548277e8ac94e78b3b1797
QiQianChen <Qiqianc@nycstudents.net>
2025-11-20 14:59:41 -0500

    .

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
+import java.util.Random;
 public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+    private final Random random = new Random();
+    public FrogSimulation(int dist, int numHops) {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+    private int hopDistance() {
+        return random.nextInt(101) - 50;
+    }
+    public boolean simulate() {
+        int position = 0;
+        for (int i = 0; i < maxHops; i++) {
+            position += hopDistance();
+            if (position >= goalDistance) {
+                return true;
+            }
+            if (position < 0) {
+                return false;
+            }
+        }
+        return false;
+    }
+    public double runSimulations(int num) {
+        int successes = 0;
+        for (int i = 0; i < num; i++) {
+            if (simulate()) {
+                successes++;
+            }
+        }
+        return (double) successes / num;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        boolean singleResult = sim.simulate();
+        System.out.println(singleResult);
+        int numRuns = 400;
+        double proportion = sim.runSimulations(numRuns);
+        System.out.println(proportion);
+    }
 }
+
